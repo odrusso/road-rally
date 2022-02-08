db0d0f23ede4e17ed16c25941ab3601d1ec77cc1
Oscar Russo <odrusso1@gmail.com>
2022-02-09 10:56:49 +1300

    add countdown timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import {Button, Typography} from "@mui/material";
+import { useRouter } from "next/dist/client/router";
 import Link from 'next/link'
 import {useEffect, useState} from "react";
+import Countdown from 'react-countdown';
+
+const countdownRenderer = ({days, hours, minutes, seconds}) => {
+    return <span>{days}d {hours}h {minutes}m {seconds}s</span>;
+};
 
 export default function Home() {
 
     const [teamName, setTeamName] = useState<string | null>(null)
+    const [startOfEventTime] = useState(Date.parse('12 Apr 2022 07:45:00'))
+    const [joinEnabled, setJoinEnabled] = useState<Boolean>(false)
 
     useEffect(() => {
         setTeamName(localStorage.getItem("teamName"))
@@ -14,6 +22,14 @@ export default function Home() {
         <>
             <Typography component={"h1"} variant={"h5"} sx={{marginBottom: 4}}>
                 🍔 Maccas Road Rally 2022 🍔
+            </Typography>
+
+            <Typography>
+                Time until event: &nbsp;
+                <strong><Countdown date={startOfEventTime}
+                                   renderer={countdownRenderer}
+                                   onComplete={() => setJoinEnabled(true)}/>
+                </strong>
             </Typography>
 
             <Link href={"/leaderboard"}>
@@ -29,7 +45,8 @@ export default function Home() {
                     <Link href={'/join'}>
                         <Button fullWidth
                                 variant="contained"
-                                sx={{mt: 3, mb: 1}}>
+                                sx={{mt: 3, mb: 1}}
+                                disabled={!joinEnabled}>
                             Join as team
                         </Button>
                     </Link>
