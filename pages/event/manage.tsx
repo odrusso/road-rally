93c25f93296fea4a4e036dd32e366886f279f04a
Oscar <odrusso1@gmail.com>
2022-04-21 22:22:16 +1200

    Add some more info the event management page

--- pages/event/manage.tsx
+++ pages/event/manage.tsx
@@ -1,11 +1,12 @@
 import {Box} from "@mui/system";
-import {Typography} from "@mui/material";
+import {Alert, Button, Typography} from "@mui/material";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
 import Countdown from "react-countdown";
 import {countdownRenderer} from "../../lib/countdownRenderer";
 import {EventInfoContext, EventInfoProvider} from "../../lib/EventInfoContext";
 import {useContext} from "react";
+import Link from "next/link";
 
 
 function ManageEvent() {
@@ -17,7 +18,12 @@ function ManageEvent() {
 
             <Box>
                 <Typography
-                    sx={{color: 'darkgray', marginBottom: 2}}>Manage {info.name}</Typography>
+                    sx={{color: 'darkgray', marginBottom: 2}}>Manage {info.name}
+                </Typography>
+
+                <Alert severity="success">Your team code is <strong>{info.code}</strong></Alert>
+
+                <br/>
 
                 <Typography>
                     Time until event: &nbsp;
@@ -26,6 +32,14 @@ function ManageEvent() {
                                        overtime={true}/>
                     </strong>
                 </Typography>
+
+                <Link href={`/event/dashboard?eventCode=${info.code}`}>
+                    <Button fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}>
+                        Dashboard
+                    </Button>
+                </Link>
 
             </Box>
 
